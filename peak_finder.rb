9ffcb055abd5bb6a99db233fb6db518ac2c9c11d
rhuberdeau <rhuberdeau@gmail.com>
2016-03-07 00:07:07 -0500

    Cleaned up a little

--- peak_finder.rb
+++ peak_finder.rb
@@ -1,30 +1,21 @@
-# array = [6,7,4,3,2,1,4,5]
+array = [6,7,4,3,2,1,4,5]
 # array = [5,4,1,2,3,4,7,6]
-array   = [1, 3, 4, 3, 5, 1, 3]
+# array   = [1, 3, 4, 3, 5, 1, 3]
 # find middle
 # check if it's peak
 # move to the side with the larger value
 
-def find_middle(array)
-  mid = array[array.length / 2]
-  array.index(mid)
-end
-
-def larger_side(index, array)
-  if array[index -1] && array[index] < array[index - 1]
-    array = array[0..index-1]
-    index = find_middle(array)
-    larger_side(index ,array)
-  elsif array[index + 1] && array[index] < array[index + 1]
-    array = array[index+1..array.length-1]
-    index = find_middle(array)
-    larger_side(index ,array)
+def find_peak(array)
+  return array.sort.last if array.length <= 2
+  
+  if array[array.length/2 ] < array[array.length/2 - 1]
+    find_peak(array[0..array.length/2 - 1])
+  elsif array[array.length/2] < array[array.length/2 + 1]
+    find_peak(array[array.length/2+1..array.length-1])
   else
-    array[index]
+    array[array.length/2]
   end
 end
 
-
-middle = find_middle(array)
-peak   = larger_side(middle, array)
+peak = find_peak(array)
 puts peak
